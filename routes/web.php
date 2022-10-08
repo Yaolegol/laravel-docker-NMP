bf25b144ea2e6d63b2ffb508bf80eeeeb3f8bce3
Oleg <m160160@yandex.ru>
2022-10-09 06:17:57 +0700

    add user data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $usersData = User::all()->toArray();
+
+    return view('index', [
+        'usersDataList' => $usersData,
+    ]);
 });
