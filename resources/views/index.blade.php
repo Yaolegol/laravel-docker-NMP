a10616a11b9dad4f843088bddcd566274a0e52d4
Oleg <m160160@yandex.ru>
2022-10-09 11:31:03 +0700

    change view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,8 +21,12 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <div
+            class="flex justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0"
+            style="flex-direction: column;"
+        >
+            <div style="font-weight: 800;">Users from DB:</div>
+            <div class="mt-8 max-w-6xl mx-auto sm:px-6 lg:px-8">
                 @foreach($usersDataList as $usersData)
                     <div>{{$usersData['name']}}</div>
                 @endforeach
